457d73fb0b52647bc97599cd2301a6cfeb010067
Eunhee Jo <joeun2ce@gmail.com>
2019-05-13 11:41:04 +0900

    Deleted activated field and Update action in token swap

--- contracts/gxc.htlc/include/gxc.htlc/gxc.htlc.hpp
+++ contracts/gxc.htlc/include/gxc.htlc/gxc.htlc.hpp
@@ -12,31 +12,23 @@ public:
 
    struct [[eosio::table]] htlc {
       name contract_name;
-      name recipient;
+      std::variant<name, checksum160> recipient;
       extended_asset value;
       checksum256 hashlock;
       time_point_sec timelock;
-      bool activated = false;
 
       uint64_t primary_key()const { return contract_name.value; }
 
-      EOSLIB_SERIALIZE(htlc, (contract_name)(recipient)(value)(hashlock)(timelock)(activated))
+      EOSLIB_SERIALIZE(htlc, (contract_name)(recipient)(value)(hashlock)(timelock))
    };
 
    typedef multi_index<"htlc"_n, htlc> htlcs;
 
-   // dummy action to resolve cdt v1.6.1 issue on notify handler
-   [[eosio::on_notify("eosio.token::transfer")]]
-   void on_eos_transfer(name from, name to, asset quantity, string memo) {
-      on_transfer(from, to, quantity, memo);
-   }
-
-   [[eosio::on_notify("*::transfer")]]
-   void on_transfer(name from, name to, asset quantity, string memo);
-   typedef action_wrapper<"transfer"_n, &htlc_contract::on_transfer> transfer_action;
+   void transfer(name from, name to, extended_asset value, string memo) {}
+   typedef action_wrapper<"transfer"_n, &htlc_contract::transfer> transfer_action;
 
    [[eosio::action]]
-   void newcontract(name owner, name contract_name, name recipient, extended_asset value, checksum256 hashlock, time_point_sec timelock);
+   void newcontract(name owner, name contract_name, std::variant<name, checksum160> recipient, extended_asset value, checksum256 hashlock, time_point_sec timelock);
 
    [[eosio::action]]
    void withdraw(name owner, name contract_name, checksum256 preimage);
