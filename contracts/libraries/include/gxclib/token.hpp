397e54cd2a1ba1d5cbbeca841644ec8abdeabb84
Jeeyong Um <conr2d@gmail.com>
2019-05-26 22:27:05 +0900

    Use xxh64 instead of fasthash64 for primary key calculation

--- contracts/libraries/include/gxclib/token.hpp
+++ contracts/libraries/include/gxclib/token.hpp
@@ -34,7 +34,11 @@ asset get_balance(name owner, name issuer, symbol_code sym_code) {
    asset balance;
    auto esc = extended_symbol_code(sym_code, issuer);
    db_get_i64(db_find_i64(token_account.value, issuer.value, "accounts"_n.value,
+#ifdef TARGET_TESTNET
                           fasthash64(reinterpret_cast<const char*>(&esc), sizeof(uint128_t))),
+#else
+                          xxh64(reinterpret_cast<const char*>(&esc), sizeof(uint128_t))),
+#endif
               reinterpret_cast<void*>(&balance), sizeof(asset));
    return balance;
 }
