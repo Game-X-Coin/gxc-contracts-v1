f27af90bec8d108c06705e137c6edf5c77cadf8c
Jeeyong Um <conr2d@gmail.com>
2019-06-10 16:45:28 +0900

    Add bancor contract

---
+++ contracts/gxc.bancor/include/gxc.bancor/gxc.bancor.hpp
@@ -0,0 +1,68 @@
+#include <eosio/singleton.hpp>
+#include "connector.hpp"
+
+using namespace eosio;
+using std::string;
+
+namespace gxc {
+
+class [[eosio::contract("gxc.bancor")]] bancor_contract : public contract {
+public:
+   using contract::contract;
+
+   struct [[eosio::table]] connector : eosio::connector {
+   };
+
+   struct base_config {
+      extended_asset connected; // if amount != 0, it's considered as fixed amount of conversion fee
+      uint16_t rate; // permyriad
+
+      bool is_exempted()const { return connected.quantity.amount == 0 && rate == 0; }
+      extended_asset get_fee(extended_asset value)const {
+         int64_t fee = 0;
+         if (rate != 0) {
+            int64_t p = 10000 / rate;
+            fee = (value.quantity.amount + p - 1) / p;
+         }
+         fee += connected.quantity.amount;
+         return {fee, value.get_extended_symbol()};
+      }
+
+      EOSLIB_SERIALIZE(base_config, (connected)(rate))
+   };
+
+   struct [[eosio::table]] charge_policy : base_config {
+      extended_symbol smart;
+
+      uint64_t primary_key() const { return smart.get_symbol().code().raw(); }
+
+      EOSLIB_SERIALIZE_DERIVED(charge_policy, base_config, (smart))
+   };
+
+   struct [[eosio::table]] config : base_config {
+      name owner;
+
+      EOSLIB_SERIALIZE_DERIVED(config, base_config, (owner))
+   };
+
+   typedef multi_index<"connector"_n, connector> connectors;
+   typedef multi_index<"charge"_n, charge_policy> charges;
+   typedef singleton<"config"_n, config> configuration;
+
+   [[eosio::action]]
+   void convert(name sender, extended_asset from, extended_asset to);
+
+   [[eosio::action]]
+   void init(name owner, extended_symbol connected);
+
+   [[eosio::action]]
+   void connect(extended_symbol smart, extended_asset balance, double weight);
+
+   [[eosio::action]]
+   void setcharge(int16_t rate, std::optional<extended_asset> fixed, std::optional<extended_symbol> smart);
+
+   [[eosio::action]]
+   void setowner(name owner);
+};
+
+} /// namespace gxc
---
+++ contracts/gxc.bancor/src/gxc.bancor.cpp
@@ -0,0 +1,155 @@
+#include <gxc.bancor/gxc.bancor.hpp>
+
+namespace gxc {
+
+constexpr name null_account = "gxc.null"_n;
+
+void bancor_contract::convert(name sender, extended_asset from, extended_asset to) {
+   require_auth(sender);
+
+   configuration cfg(_self, _self.value);
+   check(cfg.exists(), "contract not initialized");
+
+   if (from.get_extended_symbol() == cfg.get().connected.get_extended_symbol()) {
+      // initialize connector or buy smart
+      connectors conn(_self, to.contract.value);
+      auto it = conn.find(to.quantity.symbol.code().raw());
+      check(it != conn.end(), "connector not exists");
+
+      conn.modify(it, same_payer, [&](auto& c) {
+         charges chrg(_self, to.contract.value);
+         auto cit = chrg.find(to.quantity.symbol.code().raw());
+         auto fee = cit != chrg.end() ? cit->get_fee(from)
+                  : !cfg.get().is_exempted() ? cfg.get().get_fee(from)
+                  : extended_asset(0, from.get_extended_symbol());
+
+         auto quant_after_fee = from - fee;
+         check(quant_after_fee.quantity.amount > 0, "paid token not enough after charging fee");
+
+         auto smart_issued = c.convert_to_smart(quant_after_fee, to.get_extended_symbol());
+         check(smart_issued.value.quantity.amount > 0, "paid token not enough after charging fee");
+
+         auto refund = extended_asset{int64_t(quant_after_fee.quantity.amount * (1 - smart_issued.ratio)), quant_after_fee.get_extended_symbol()};
+         if (refund.quantity.amount > 0) {
+            auto overcharged = int64_t(fee.quantity.amount * (1 - smart_issued.ratio));
+            if (overcharged < 0) overcharged = 0;
+            fee.quantity.amount -= overcharged;
+            refund.quantity.amount += overcharged;
+         }
+         token(from.contract, _self).transfer(sender, _self, from - refund);
+         if (fee.quantity.amount > 0) {
+            token(from.contract, _self).transfer(_self, cfg.get().owner, fee, "conversion fee");
+         }
+         token(to.contract).transfer(null_account, sender, smart_issued.value);
+         //print("effective_price = ", asset((from.quantity.amount - (fee.quantity.amount + refund.amount)) / smart_issued.value.quantity.amount * pow(10, smart_issued.value.quantity.symbol.precision()), quantity.symbol));
+      });
+   } else {
+      // sell smart
+      connectors conn(_self, from.contract.value);
+      auto it = conn.find(from.quantity.symbol.code().raw());
+      check(it != conn.end(), "connector not exists");
+
+      conn.modify(it, same_payer, [&](auto& c) {
+         auto connected_out = c.convert_from_smart(from, cfg.get().connected.get_extended_symbol());
+
+         charges chrg(_self, from.contract.value);
+         auto cit = chrg.find(from.quantity.symbol.code().raw());
+         auto fee = cit != chrg.end() ? cit->get_fee(connected_out.value)
+                  : !cfg.get().is_exempted() ? cfg.get().get_fee(connected_out.value)
+                  : extended_asset(0, connected_out.value.get_extended_symbol());
+
+         auto quant_after_fee = connected_out.value - fee;
+         check(quant_after_fee.quantity.amount > 0, "paid token not enough after charging fee");
+
+         auto refund = extended_asset{int64_t(from.quantity.amount * (1 - connected_out.ratio)), from.get_extended_symbol()};
+         token(from.contract, _self).transfer(sender, _self, from - refund);
+         token(from.contract, _self).transfer(_self, null_account, from - refund);
+         token(cfg.get().connected.contract, _self).transfer(_self, sender, quant_after_fee);
+         if (fee.quantity.amount > 0) {
+            token(cfg.get().connected.contract, _self).transfer(_self, cfg.get().owner, fee, "conversion fee");
+         }
+         //print("effective_price = ", asset(quant_after_fee.quantity.amount / (from.quantity.amount - refund.amount) * pow(10, from.quantity.symbol.precision()), connected_out.value.quantity.symbol));
+      });
+   }
+}
+
+void bancor_contract::init(name owner, extended_symbol connected) {
+   require_auth(_self);
+
+   configuration cfg(_self, _self.value);
+   check(!cfg.exists(), "already initialized");
+   cfg.set({extended_asset{0, connected}, 0, owner}, _self);
+}
+
+void bancor_contract::connect(extended_symbol smart, extended_asset balance, double weight) {
+   require_auth(_self);
+
+   configuration cfg(_self, _self.value);
+   check(cfg.exists(), "contract not initialized");
+   check(cfg.get().connected.get_extended_symbol() == balance.get_extended_symbol(), "balance should be paid by connected token");
+
+   connectors conn(_self, smart.get_contract().value);
+   auto it = conn.find(smart.get_symbol().code().raw());
+   check(it == conn.end(), "existing connector");
+   conn.emplace(_self, [&](auto& c) {
+      c.smart = smart;
+      c.balance = balance.quantity;
+      c.weight = weight;
+   });
+
+   token(balance.contract, _self).transfer(cfg.get().owner, _self, balance);
+}
+
+void bancor_contract::setcharge(int16_t rate, std::optional<extended_asset> fixed, std::optional<extended_symbol> smart) {
+   require_auth(_self);
+
+   configuration cfg(_self, _self.value);
+   check(cfg.exists(), "initialize contract before setting charge");
+   check(!fixed || cfg.get().connected.get_extended_symbol() == fixed->get_extended_symbol(), "represent conversion fee in connected token");
+
+   if (!smart) {
+      check(rate >= 0 && rate <= 1000, "rate needs to be in the range of 0-1000 (per mille)");
+      auto it = cfg.get();
+      it.rate = static_cast<uint16_t>(rate);
+      if (fixed)
+         it.connected = *fixed;
+      cfg.set(it, _self);
+   } else {
+      charges chrg(_self, smart->get_contract().value);
+      auto it = chrg.find(smart->get_symbol().code().raw());
+
+      if (rate == -1) {
+         check(it != chrg.end(), "no charge policy to be deleted");
+         chrg.erase(it);
+         return;
+      }
+
+      check(rate >= 0 && rate <= 1000, "rate needs to be in the range of 0-1000 (per mille)");
+      if (it == chrg.end()) {
+         chrg.emplace(_self, [&](auto& c) {
+            c.smart = *smart;
+            c.rate = static_cast<uint16_t>(rate);
+            c.connected = fixed ? *fixed : extended_asset(0, cfg.get().connected.get_extended_symbol());
+         });
+      } else {
+         chrg.modify(it, same_payer, [&](auto& c) {
+            c.rate = static_cast<uint16_t>(rate);
+            if (fixed)
+               c.connected = *fixed;
+         });
+      }
+   }
+}
+
+void bancor_contract::setowner(name owner) {
+   require_auth(_self);
+
+   configuration cfg(_self, _self.value);
+   check(cfg.exists(), "initialize contract before setting owner");
+
+   auto it = cfg.get();
+   it.owner = owner;
+   cfg.set(it, _self);
+}
+
+} /// namespace gxc
